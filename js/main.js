7a2c97a1ca2231abb87a6fe7bf08d03dbaa0b6df
pooja <aishajadhav02@gmail.com>
2024-10-09 20:48:32 +0530

    tenant latest new design changes done

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ $(document).ready(function(){
 	  $(".view_all").hide();
 	});
   });
+
+//   $('.slidermaintenance').slick({
+// 	slidesToShow: 3,
+// 	slidesToScroll: 1,
+// 	asNavFor: '.slider-for',
+// 	dots: true,
+// 	focusOnSelect: true
+//   });
+
+
+// additional income and employee
+$(".accord_one").hide();
+$(document).ready(function() {
+	$(".btn.btnadd_emp").click(function(){
+		$(".accord_one").show();
+	  });     
+});
+
+$(".quesimg").mouseenter(function() {
+	$(".question_tooltipcontent").fadeIn(10); 
+ });
+ 
+$(".quesimg").mouseleave(function() {
+	$(".question_tooltipcontent").fadeOut(10);   
+   });
